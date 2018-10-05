1e53bbfa20e0d84b0862abe63ddcae3060767408
Lucas Tomkiel <lucas.tomkiel@gmail.com>
2018-10-05 13:20:11 -0300

    Create project

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './list/item/item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    ItemComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
